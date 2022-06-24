7d9291e78afeb26bff80bef5a1b639ccfe9fcc91
Shelery <eszter.with.a.z@gmail.com>
2022-06-24 17:56:37 +0200

    05 and 06 - solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,23 @@
 const sumAll = function(intStart, intEnd) {
-    let sum = 0;
+    
     if (Number.isInteger(intStart) && Number.isInteger(intEnd)){
-        
-        if (intEnd > intStart){
-            for (let i = intStart; i <= intEnd; i++){
-                sum += i;
+        if (intStart >= 0 && intEnd >= 0){
+            let sum = 0;
+            if (intEnd > intStart){
+                for (let i = intStart; i <= intEnd; i++){
+                    sum += i;
+                }
+            } else if (intStart > intEnd){
+                for (let i = intEnd; i <= intStart; i++){
+                    sum+=i;
+                } 
+            } else {
+                sum = intStart;
             }
-        } else if (intStart > intEnd){
-            for (let i = intEnd; i <= intStart; i--){
-                sum+=i;
-            } 
-        } else {
-            sum = intStart;
-        }
+            return sum;
 
-
-
-    }
-    return sum;
+        } else return "ERROR";
+    } else return "ERROR";
 };
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year)){
+        if (year % 4 === 0){
+            if (year % 100 === 0){
+                if (year % 400 === 0){
+                    return true
+                }
+            } else return true
+        }
+    return false
+    } else return `ERROR`
 };
 
 // Do not edit below this line
+
 module.exports = leapYears;
