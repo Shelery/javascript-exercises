70e5d78af808504f003808b867c498e4131fd687
Shelery <eszter.with.a.z@gmail.com>
2022-06-24 18:26:03 +0200

    07 - solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  celsius = (fahrenheit-32) * (5/9)
+  return roundToOneDecimal(celsius);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  fahrenheit = celsius*(9/5) + 32
+  return roundToOneDecimal(fahrenheit);
 };
+
+function roundToOneDecimal(float){
+  return Math.round(float * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
