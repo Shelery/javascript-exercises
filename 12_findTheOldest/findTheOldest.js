cbdfaab76f54b9e493712b5b7f517c446715f9b2
Shelery <eszter.with.a.z@gmail.com>
2022-07-16 18:48:21 +0200

    12 - solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(people) {
+//     people = [ {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011, - optional
+//       },... ]
+
+    const oldest = people.reduce((oldestPersonFound, currentPerson) => {
+        let highestAge = findAge(oldestPersonFound);
+        let currentAge = findAge(currentPerson);
+        if (currentAge > highestAge){
+            oldestPersonFound = currentPerson;
+        };
+        return oldestPersonFound
+    })
+
+    function findAge(person){
+        // Check if person is dead
+        if ("yearOfDeath" in person ){
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+        // If not: get current age
+        const currentYear = new Date().getFullYear();
+        return currentYear - person.yearOfBirth;
+    }
+    return oldest;
 };
 
 // Do not edit below this line
